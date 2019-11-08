f6d116aca56d55e4a943ac93ddbf29c66795cf31
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-08 10:40:33 -0800

    update branch logic, user interface.

--- Desktop/pizza-shop/js/scripts.js
+++ Desktop/pizza-shop/js/scripts.js
@@ -1,20 +1,20 @@
-function Pizza(topping, size, price) {
+function Pizza(topping, size) {
   this.topping = topping;
   this.size = size;
   this.price = 0;
 }
 // Logic
 Pizza.prototype.getPrice = function() {
-  this.topping.forEach(function(topping, size, price){
+  this.topping.forEach(function(topping){
     this.price += 1;
-    if(size === "BigBoi"){
+    if(this.size === "BigBoi"){
       this.price += 20;
     }
-    else if(size === "SmallBoi"){
+    else if(this.size === "SmallBoi"){
       this.price += 10;
     }
     return this.price;
-  })
+  });
 };
 
 
@@ -23,12 +23,18 @@ Pizza.prototype.getPrice = function() {
 $(document).ready(function(){
   $("#toppingsForm").submit(function(event){
     event.preventDefault();
-    let toppingInput = $("input:checkbox[name=toppings]:checked");
+    let toppingInput = $("input:checkbox[name=topping]:checked")
     let toppingArray = [];
-    let size = $("#size").val();
-    $(toppingInput).each(function(){
+    let sizeInput = $("input:checkbox[name=size]:checked")
+
+    toppingInput.each(function(){
       toppingArray.push($(this).val());
-      $('#userOrder').append(toppingArray);
+      console.log(toppingArray);
+      $('#userOrder').append(toppingArray + "<br>");
     });
+
+    let myPizza = new Pizza (sizeInput, toppingArray);
+    let price = myPizza.getPrice();
+    console.log(myPizza);
   });
 });
