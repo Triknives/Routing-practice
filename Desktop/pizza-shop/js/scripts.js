4c14c7ce183117d968abbd08f87cfe019472d743
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-08 09:30:04 -0800

    update branch logic, user interface.

--- Desktop/pizza-shop/js/scripts.js
+++ Desktop/pizza-shop/js/scripts.js
@@ -1,12 +1,22 @@
+function Pizza(topping, price) {
+  this.topping = topping;
+  this.size = size;
+  this.price = 0;
+}
+// Logic
+Pizza.prototype.getPrice = function() {
+  this.topping.forEach(function(topping, size){
+    this.price += 1;
 
-
-
-
-
-
-
-
-
+    if(size === "BigBoi"){
+      this.price += 20;
+    }
+    else if(size === "SmallBoi"){
+      this.price += 10;
+    }
+    return this.price;
+  })
+};
 
 
 
@@ -14,3 +24,9 @@
 $(document).ready(function(){
   $("#toppingsForm").submit(function(event){
     event.preventDefault();
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var toppingsInput = $(this).val();
+      $('#userOrder').append(toppingsInput + "<br>");
+    });
+  });
+});
