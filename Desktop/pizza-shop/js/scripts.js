7dd859985fa3c9c0f7d2e855ad8276bb13a0c637
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-10 20:01:01 -0800

    update to styling, added images

--- Desktop/pizza-shop/js/scripts.js
+++ Desktop/pizza-shop/js/scripts.js
@@ -46,6 +46,7 @@ $(document).ready(function(){
     $("#pizzaSize").append(size);
     $("#orderTotal").text(price);
     $("#toppingsForm").hide();
+    $("#specials").hide();
     $("#orderReveal").show();
 
   })
