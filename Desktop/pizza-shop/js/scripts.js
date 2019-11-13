e4714b710e8b4a170ef72bbc7fe90b78b94017fb
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-12 19:58:19 -0800

    removed log checks from js

--- Desktop/pizza-shop/js/scripts.js
+++ Desktop/pizza-shop/js/scripts.js
@@ -1,10 +1,8 @@
-// Declaring Object and Keys
 function Pizza(topping, size) {
   this.topping = topping;
   this.size = size;
   this.price = 0;
 }
-// Branch logic
 Pizza.prototype.getPrice = function(topping) {
   for(var i = 0; i < this.topping.length; i++){
     this.price += 1;
@@ -16,26 +14,18 @@ Pizza.prototype.getPrice = function(topping) {
     this.price += 10;
   }
   else
-  console.log(this.price);
     return this.price;
 };
-
-
-//User Interface Logic
 $(document).ready(function(){
   $("#toppingsForm").submit(function(event){
     event.preventDefault();
     let toppingInput = $("input:checkbox[name=topping]:checked");
     let toppingArray = [];
     let size = $("#size").val();
-
-
-
     $("input:checkbox[name=topping]:checked").each(function(){
       let userToppings = $(this).val();
       $("#userOrder").append(userToppings + "<br>");
     })
-
     toppingInput.each(function(){
       toppingArray.push($(this).val());
     })
@@ -50,6 +40,5 @@ $(document).ready(function(){
     $("#intro").hide();
     $("#orderReveal").show();
     $("#thankYou").show();
-
   })
 });
